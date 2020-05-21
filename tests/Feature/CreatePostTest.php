78f0535187ba5f7abb699f9c01bc3717e7554b3a
David Peach <mail@davidpeach.co.uk>
2020-05-21 08:14:36 +0100

    WIP

---
+++ tests/Feature/CreatePostTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CreatePostTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function a_post_can_be_created()
+    {
+        //
+    }
+}
